f334ff2c3d5bf80923c242caae02674387a30b5c
Gerin Aryo <gerinaryo@Gerins-MacBook-Pro.local>
2022-04-13 01:41:25 +0700

    create login and register controller in AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,89 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Controllers\BaseController;
+use App\Models\Admin;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    //user register
+    public function registerUser(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required',
+            'email' => 'required|unique:users, email',
+            'password' => 'required',
+            'age' => 'required',
+            'location' => 'required',
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password']),
+        ]);
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token,
+        ];
+        return response($response, 201);
+    }
+
+    public function loginUser(Request $request)
+    {
+        //validation request on body
+        $fields = $request->validate([
+            'email' => 'required|string', //uniqe terhadap tabel users dan field email
+            'password' => 'required|string',
+        ]);
+
+        //check email
+        $user = User::where('email', $fields['email'])->first(); //query
+
+        //check password
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Bad creds'
+            ], 401);
+        }
+        $token = $user->createToken('token')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token,
+        ];
+        return response($response, 201);
+    }
+
+    public function loginAdmin(Request $request)
+    {
+        //validation request on body
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        //check email
+        $admin = Admin::where('email', $fields['email'])->first(); //query
+
+        //check password
+        if (!$admin || !Hash::check($fields['password'], $admin->password)) {
+            return response([
+                'message' => 'Bad creds'
+            ], 401);
+        }
+        $token = $admin->createToken('token')->plainTextToken;
+
+        $response = [
+            'admin' => $admin,
+            'token' => $token,
+        ];
+        return response($response, 201);
+    }
 }
